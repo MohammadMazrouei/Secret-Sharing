ed108c31d9af1b7acfe82783288f4b606715f221
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-06-14 14:36:46 +0330

    add Mint class & generate keys & generate sercet

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "main.h"
 // TODO use getline for get text and string keys
 
+std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+
 std::string get_type() {
     std::string type;
     while (true) {
@@ -44,23 +46,7 @@ int64_t input_mod() {
     return mod;
 }
 
-std::string input_text() {
-    std::string text;
-    std::cout << "Enter text: " << std::endl;
-    std::cin >> text;
-    return text;
-}
-
-std::vector<std::pair<int, std::string>> input_string_keys(int t) {
-    std::vector<std::pair<int, std::string>> keys(t);
-    for (int i = 0; i < t; i++) {
-        std::cout << "Enter No, key " << i + 1 << ": "; 
-        std::cin >> keys[i].first >> keys[i].second;
-    }
-    return keys;
-}
-
-std::vector<std::pair<int, int64_t>> input_number_keys(int t) {
+std::vector<std::pair<int, int64_t>> input_keys(int t) {
     std::vector<std::pair<int, int64_t>> keys(t);
     for (int i = 0; i < t; i++) {
         std::cout << "Enter No, key " << i + 1 << ": "; 
@@ -73,19 +59,143 @@ void print_line() {
     std::cout << BLUE << "\n----------------------------------------------------------------" << WHITE << std::endl;
 }
 
-template <typename T>
+int64_t MOD = 1e9 + 7;
+class Mint {
+    private:
+        int64_t v; 
+
+    public:
+        Mint() { 
+            v = 0; 
+        }
+        Mint(int64_t _v) {
+            v = (-MOD < _v && _v < MOD) ? _v : _v % MOD;
+            if (v < 0) {
+                v += MOD;
+            }
+        }
+
+        explicit operator int64_t() const { 
+            return v;
+        }
+        explicit operator int() const { 
+            return v;
+        }
+
+        friend bool operator == (const Mint &a, const Mint &b) { 
+            return a.v == b.v;
+        }
+        friend bool operator != (const Mint &a, const Mint &b) { 
+            return !(a == b);
+        }
+        friend bool operator < (const Mint &a, const Mint &b) { 
+            return a.v < b.v;
+        }
+        friend bool operator > (const Mint &a, const Mint &b) { 
+            return a.v > b.v;
+        }
+        friend bool operator <= (const Mint &a, const Mint &b) { 
+            return a.v <= b.v;
+        }
+        friend bool operator >= (const Mint &a, const Mint &b) { 
+            return a.v >= b.v;
+        }
+
+        friend Mint operator + (Mint a, const Mint &b) { 
+            return a += b;
+        }
+        friend Mint operator - (Mint a, const Mint &b) { 
+            return a -= b;
+        }
+        friend Mint operator * (Mint a, const Mint &b) { 
+            return a *= b;
+        }
+        friend Mint operator / (Mint a, const Mint &b) { 
+            return a /= b;
+        }
+        Mint& operator += (const Mint &m) { 
+            if ((v += m.v) >= MOD) {
+                v -= MOD;
+            }
+            return *this;
+        }
+        Mint& operator -= (const Mint &m) { 
+            if ((v -= m.v) < 0) {
+                v += MOD; 
+            }
+            return *this; 
+        }
+        Mint& operator *= (const Mint &m) { 
+            v = v * m.v % MOD;
+            return *this; 
+        }
+        Mint& operator /= (const Mint &m) { 
+            return (*this) *= inv(m); 
+        }
+
+        Mint operator - () const { 
+            return Mint(-v);
+        }
+        Mint& operator ++ () { 
+            return *this += 1;
+        }
+        Mint& operator -- () { 
+            return *this -= 1;
+        }
+        Mint operator ++ (int) { 
+            Mint temp; 
+            temp.v = v++; 
+            return temp; 
+        }
+        Mint operator -- (int) { 
+            Mint temp; 
+            temp.v = v--; 
+            return temp; 
+        }
+
+        friend Mint pow(Mint a, int64_t b) {
+            assert(b >= 0);
+            Mint res = 1;
+            while (b) {
+                if (b & 1) {
+                    res *= a;
+                }
+                a *= a;
+                b >>= 1;
+            }
+            return res;
+        }
+        friend Mint inv(const Mint &a) {
+            assert(a.v != 0);
+            return pow(a, MOD - 2);
+        }
+
+        friend std::ostream& operator << (std::ostream &os, const Mint &m) {
+            os << m.v; 
+            return os;
+        }
+        friend std::istream& operator >> (std::istream &is, Mint &m) {
+            int64_t x; 
+            is >> x;
+            m.v = x;
+            return is;
+        }
+};
+
 class Encoder {
     private:
         int s, t;
-        T secret, mod;
-        std::vector<T> keys;
+        int64_t secret, mod;
+        std::vector<std::pair<int, int64_t>> keys;
 
     public:
-        Encoder(int _s, int _t, T _secret, T _mod = T()) {
+        Encoder(int _s, int _t, int64_t _secret, int64_t _mod = int64_t()) {
             s = _s;
             t = _t;
             secret = _secret;
             mod = _mod;
+            MOD = mod;
+            keys.resize(s);
         }
 
         int get_s() {
@@ -100,41 +210,58 @@ class Encoder {
         void set_t(int _t) {
             t = _t;
         }
-        T get_secret() {
+        int64_t get_secret() {
             return secret;
         }
-        void set_secret(T _secret) {
-            secret = _secret();
+        void set_secret(int64_t _secret) {
+            secret = _secret;
         }
-        T get_mod() {
+        int64_t get_mod() {
             return mod;
         }
-        void set_mod(T _mod) {
+        void set_mod(int64_t _mod) {
             mod = _mod;
+        }
+
+        int64_t f(int x, std::vector<int> coefficients) {
+            Mint mi = secret;
+            for (int i = 0; i < t - 1; i++) {
+                mi += coefficients[i] * pow(Mint(x), i + 1); 
+            }
+
+            return int64_t(mi);
         }
 
         void generate_keys() {
-
+            std::vector<int> coefficients(t - 1);
+            for (int i = 0; i < t - 1; i++) {
+                coefficients[i] = uid(1, 1000000000);
+            }
+            for (int i = 0; i < s; i++) {
+                keys[i] = {i + 1, f(i + 1, coefficients)};
+                std::cout << keys[i].first << ' ' << keys[i].second << std::endl;
+            }
         }
 };
 
-template <typename T>
 class Decoder {
     private:
         int t;
-        T secret, mod;
-        std::vector<std::pair<int, T>> keys;
+        int64_t secret, mod;
+        std::vector<std::pair<int, int64_t>> keys;
 
     public:
-        Decoder(int _t, std::vector<std::pair<int, T>> _keys, T _mod = T()) {
+        Decoder(int _t, std::vector<std::pair<int, int64_t>> _keys, int64_t _mod) {
             t = _t;
             keys = _keys;
             mod = _mod;
+            MOD = mod;
         }
-        Decoder(std::vector<T> _keys, T _mod = T()) {
+        Decoder(std::vector<std::pair<int, int64_t>> _keys, int64_t _mod) {
             t = (int)keys.size();
             keys = _keys;
             mod = _mod;
+            MOD = mod;
         }
 
         int get_t() {
@@ -143,21 +270,30 @@ class Decoder {
         void set_t(int _t) {
             t = _t;
         }
-        std::vector<std::pair<int, T>> get_keys() {
+        std::vector<std::pair<int, int64_t>> get_keys() {
             return keys;
         }
-        void set_keys(std::vector<std::pair<int, T>> _secret) {
-            secret = _secret();
+        void set_keys(std::vector<std::pair<int, int64_t>> _keys) {
+            keys = _keys;
         }
-        T get_mod() {
+        int64_t get_mod() {
             return mod;
         }
-        void set_mod(T _mod) {
+        void set_mod(int64_t _mod) {
             mod = _mod;
         }
 
         void generate_secret() {
-
+            Mint mi = 0;
+            for (int i = 0; i < t; i++) {
+                for (int j = 0; j < t; j++) {
+                    if (i != j) {
+                        mi += keys[i].second * (keys[j].first / (keys[j].first - keys[i].first));
+                    }
+                }
+            }
+            secret = int64_t(mi);
+            std::cout << mi << std::endl;
         }
 };
 
@@ -168,43 +304,21 @@ int main(int argc, char* argv[]) {
     }
 
     if (option == ENCODE_OPTION) {
-        std::string type = get_type();
-        if (type == "string") {
-            int s = input_s(), t = input_t();
-            std::string secret = input_text();
-            print_line();
+        int s = input_s(), t = input_t();
+        int64_t secret = input_number(), mod = input_mod();
+        print_line();
 
-            Encoder<std::string> encoder(s, t, secret);
-            encoder.generate_keys();
-        }
-        else if (type == "number") {
-            int s = input_s(), t = input_t();
-            int64_t secret = input_number(), mod = input_mod();
-            print_line();
-
-            Encoder<int64_t> encoder(s, t, secret, mod);
-            encoder.generate_keys();
-        }
+        Encoder encoder(s, t, secret, mod);
+        encoder.generate_keys();
     }
     else if (option == DECODE_OPTION) {
-        std::string type = get_type();
-        if (type == "string") {
-            int t = input_t();
-            std::vector<std::pair<int, std::string>> keys = input_string_keys(t);
-            print_line();
+        int t = input_t();
+        int64_t mod = input_mod();
+        std::vector<std::pair<int, int64_t>> keys = input_keys(t);
+        print_line();
 
-            Decoder<std::string> decoder(t, keys);;
-            decoder.generate_secret();
-        }
-        else if (type == "number") {
-            int t = input_t();
-            int64_t mod = input_mod();
-            std::vector<std::pair<int, int64_t>> keys = input_number_keys(t);
-            print_line();
-
-            Decoder<int64_t> decoder(t, keys, mod);
-            decoder.generate_secret();
-        }
+        Decoder decoder(t, keys, mod);
+        decoder.generate_secret();
     }
     else {
         std::cerr << RED << "Option Error: Option is invalid." << WHITE << std::endl;
--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cassert>
+#include <chrono>
+#include <random>
 
 #define SUCCESS 0
 #define FAILIURE 1
@@ -15,3 +18,5 @@ const std::string WHITE = COLORED_OUTPUT ? "\033[0;m" : "";
 const std::string RED = COLORED_OUTPUT ? "\033[0;31m" : "";
 const std::string GREEN = COLORED_OUTPUT ? "\033[0;32m" : "";
 const std::string BLUE = COLORED_OUTPUT ? "\033[0;34m" : "";
+
+#define uid(a, b) std::uniform_int_distribution<int>(a, b)(rng)
